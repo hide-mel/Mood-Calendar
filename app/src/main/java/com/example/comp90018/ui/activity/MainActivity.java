f41eaef2d6a35762f701598edb2672d0d6766400
HannahYZhao <47459029+HannahYZhao@users.noreply.github.com>
2022-11-01 22:39:50 +1100

    Update MainActivity.java

--- app/src/main/java/com/example/comp90018/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/comp90018/ui/activity/MainActivity.java
@@ -1,2 +1,28 @@
-package com.example.comp90018.ui.activity;public class MainActivity {
+package com.example.comp90018.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private Spinner spinner1, spinner2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        User.initUsers();
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+
+        SpinnerAdapter customAdapter = new SpinnerAdapter(this, R.layout.custom_spinner_adapter, User.getUserArrayList());
+        spinner1.setAdapter(customAdapter);
+    }
+
+
 }

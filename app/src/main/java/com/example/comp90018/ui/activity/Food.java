e108bddeaf5f156992ade6634ef88bb2a23a9a0a
HannahYZhao <47459029+HannahYZhao@users.noreply.github.com>
2022-11-02 18:31:40 +1100

    Update Food.java

--- app/src/main/java/com/example/comp90018/ui/activity/Food.java
+++ app/src/main/java/com/example/comp90018/ui/activity/Food.java
@@ -33,44 +33,38 @@ public class Food
         this.name = name;
     }
 
-    public static void initHobbies()
+    public static void initFood()
     {
-        Food hobbies1 = new Food("0", "Family");
+        Food hobbies1 = new Food("0", "Eat healthy");
         userArrayList.add(hobbies1);
 
-        Food hobbies2 = new Food("1", "Friends");
+        Food hobbies2 = new Food("1", "Fast food");
         userArrayList.add(hobbies2);
 
-        Food hobbies3 = new Food("2", "Date");
+        Food hobbies3 = new Food("2", "Homemade");
         userArrayList.add(hobbies3);
 
-        Food hobbies4 = new Food("3", "Party");
+        Food hobbies4 = new Food("3", "Eat out");
         userArrayList.add(hobbies4);
 
     }
 
-    public int getImage()
-    {
-        switch (getId())
-        {
+    public int getImage() {
+        switch (getId()) {
             case "0":
-                return R.drawable.usericon0;
+                return R.drawable.foodicon0;
             case "1":
-                return R.drawable.usericon1;
-//            case "2":
-//                return R.drawable.usericon2;
-//            case "3":
-//                return R.drawable.usericon3;
-//            case "4":
-//                return R.drawable.usericon4;
-//            case "5":
-//                return R.drawable.usericon5;
-        }
-        return R.drawable.usericon0;
-    }
+                return R.drawable.foodicon1;
+            case "2":
+                return R.drawable.foodicon2;
+            case "3":
+                return R.drawable.foodicon3;
 
-    public static ArrayList<Food> getUserArrayList()
-    {
-        return userArrayList;
+            return R.drawable.foodicon0;
+        }
+
+        public static ArrayList<Food> getUserArrayList()
+        {
+            return userArrayList;
+        }
     }
-}
